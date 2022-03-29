537e5cead2cb3e4b200bae57560bf70a92fda4e0
CrisDgrnu <cris.dgrnu@gmail.com>
2022-03-29 19:17:39 +0200

    Add sidebar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+
+import React from 'react';
+import Sidebar from '../Sidebar';
+
+const Layout = () => {
+    return (
+        <>
+            <Sidebar></Sidebar>
+        </>
+    );
+};
+
+export default Layout;
