346dc575a0676b7f77d17d5d1750eca25f2c649e
CrisDgrnu <cris.dgrnu@gmail.com>
2022-04-02 16:29:10 +0200

    Logo updates

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import LogoSubtitle from '../../assets/images/logo-sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faUser,
