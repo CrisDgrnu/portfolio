0c924fd3e27ac6d90da4d2a811d082c90c736434
CrisDgrnu <cris.dgrnu@gmail.com>
2022-04-07 14:43:24 +0200

    Fix libs

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,49 +1,12 @@
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap-trial';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import LogoS from '../../../assets/images/logo-s.png';
+import { useRef } from 'react';
 import './index.scss';
 
 const Logo = () => {
     const bgRef = useRef();
     const outlineLogoRef = useRef();
-    const solidLogoRef = useRef();
-
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin);
-
-        gsap.timeline()
-            .to(bgRef.current, {
-                duration: 1,
-                opacity: 1,
-            })
-            .from(outlineLogoRef.current, {
-                drawSVG: 0,
-                duration: 5,
-            });
-
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        );
-    }, []);
 
     return (
         <div className="logo-container" ref={bgRef}>
-            {/* <img
-                className="solid-logo"
-                ref={solidLogoRef}
-                src={LogoS}
-                alt="JavaScript,  Developer"
-            /> */}
-
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 42.45 52.42">
